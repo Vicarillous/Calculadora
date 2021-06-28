8f9a589cdb1c7a4b93c7fa251775c3d7ae1065ce
Vicarillous <marquesv473@gmail.com>
2021-06-27 22:20:31 -0400

    Emc adicionado, mobile compativel

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function focado() {
     }
 }
 
-const numerosPermitidos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.', ","];
+const numerosPermitidos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.'];
 const simbolosPermitidos = ['+', '-', '*', '/'];
 
 function apagar() {
@@ -41,6 +41,7 @@ function apagar() {
         info.value = 0;
     }
 }
+
 var enterPressionado = false;
 document.addEventListener('keydown', (event) => {
     const teclaPressionada = event.key;
@@ -97,7 +98,7 @@ function inserirValor(v) {
 
     for (var i = 0; i < numerosPermitidos.length; i++) {
         if (v == numerosPermitidos[i]) {
-            if (info.value == 0) {
+            if (info.value === "0" && v != ".") {
                 info.value = v;
             } else {
                 info.value = (info.value + v);
@@ -108,28 +109,33 @@ function inserirValor(v) {
     }
 
     if (find == false) {
+        var valorContemSimbolo = false;
         for (var i = 0; i < simbolosPermitidos.length; i++) {
-            var split = (info.value).split("");
-            var isSimbolo = false;
+            if (v == simbolosPermitidos[i]){
+                valorContemSimbolo = true;
+            }
+        }
 
-            for (var x = 0; x <simbolosPermitidos.length; x++) {
-                if (split[(split.length)-2] == simbolosPermitidos[i]) {
-                    isSimbolo = true;
+        if (valorContemSimbolo == true) {      
+            var split = (info.value).split("");          
+            var isRepetido = false;
+
+            for (var i = 0; i <simbolosPermitidos.length; i++) {
+                if (split[(split.length)-2] == simbolosPermitidos[i]) {             
+                    isRepetido = true;
                 }
             }
 
-            if (isSimbolo == true) {
+            if (isRepetido == true) {
                 console.log("apaga");
                 split.pop();
                 split.pop();
                 split.pop();
             }
 
-            info.value = split.join(""); 
+            info.value = split.join("");
 
-            if (v == simbolosPermitidos[i]) {
-                info.value = info.value + " " + v + " ";
-            }
+            info.value = info.value + " " + v + " ";
         }
     }
 }
@@ -169,101 +175,111 @@ function igual() {
     separado = info.value.split(' ');
     console.log("Vetor inicial:");
     console.log(separado)
-    var soma = 0, multiplicacao = 0, res = 0;
+    var res = 0;
 
 
-    for (var i = 0; i < separado.length; i++) {
-        if (separado[i] == '/') {
-            res = parseFloat(separado[i - 1]) / parseFloat(separado[i + 1]);
-            separado[i - 1] = res;
+    if (separado.length == 1) {
+        res =  separado[0];
+    } else {
+        for (var i = 0; i < separado.length; i++) {
+            if (separado[i] == '/') {
+                res = parseFloat(separado[i - 1]) / parseFloat(separado[i + 1]);
+                separado[i - 1] = res;
 
-            separado.splice(i, 2);
-            i = 0;
+                separado.splice(i, 2);
+                i = 0;
+            }
         }
-    }
 
-    console.log("Vetores pós divisão:");
-    console.log(separado);
-    console.log(res);
+        console.log("Vetores pós divisão:");
+        console.log(separado);
+        console.log(res);
 
-    for (var i = 0; i < separado.length; i++) {
-        if (separado[i] == '*') {
-            res = parseFloat(separado[i - 1]) * parseFloat(separado[i + 1]);
-            separado[i - 1] = res;
-            separado.splice(i, 2);
-            i = 0;
+        for (var i = 0; i < separado.length; i++) {
+            if (separado[i] == '*') {
+                res = parseFloat(separado[i - 1]) * parseFloat(separado[i + 1]);
+                separado[i - 1] = res;
+                separado.splice(i, 2);
+                i = 0;
+            }
         }
-    }
 
-    console.log("Vetores pós multiplicação:");
-    console.log(separado);
-    console.log(res);
+        console.log("Vetores pós multiplicação:");
+        console.log(separado);
+        console.log(res);
 
-    for (var i = 0; i < separado.length; i++) {
-        if (separado[i] == '-') {
-            separado[i] = '+';
-            separado[i + 1] = -1 * parseFloat(separado[i + 1]);
+        for (var i = 0; i < separado.length; i++) {
+            if (separado[i] == '-') {
+                separado[i] = '+';
+                separado[i + 1] = -1 * parseFloat(separado[i + 1]);
+            }
         }
-    }
 
-    console.log("Transformado - para +");
-    console.log(separado);
+        console.log("Transformado - para +");
+        console.log(separado);
 
-    for (var i = 0; i < separado.length; i++) {
-        if (separado[i] == '+') {
-            res = parseFloat(separado[i - 1]) + parseFloat(separado[i + 1]);
-            separado[i - 1] = res;
-            separado.splice(i, 2);
-            i = 0;
+        for (var i = 0; i < separado.length; i++) {
+            if (separado[i] == '+') {
+                res = parseFloat(separado[i - 1]) + parseFloat(separado[i + 1]);
+                separado[i - 1] = res;
+                separado.splice(i, 2);
+                i = 0;
+            }
         }
-    }
 
-    console.log("Vetores pós adição:");
-    console.log(separado);
-    console.log(res);
+        console.log("Vetores pós adição:");
+        console.log(separado);
+        console.log(res);
+    }
     
 
     console.log('resultado final: ' + res);
+    addHistorico(info.value, res);
     info.value = res;
     calculado = true;
-    console.log(calculado);
+    console.log(calculado);   
 }
 
-/*function tgShowHide(toggleVisibilidade) {
+function addHistorico(equacao, res) {
+    console.log("oi");
+    var calculo = document.createElement("p");
+    var text = document.createTextNode(equacao +" = " + res);
+    calculo.appendChild(text);
+    var element = document.getElementById("historico");
+    element.appendChild(calculo);
+}
+
+//* * Parte da calculadora E = mc2 */
+
+var toggleVisibilidade = true;
+function tgShowHide() {
     var calcEmc = document.getElementById("calc-emc");
-    var menu = document.getElementById("btn-menu");
-    var calcBody =document.getElementById("calc-body");
-    if (toggleVisibilidade == false) {
-        toggleVisibilidade = true;
-
-        calcEmc.classList.remove("move");
-        menu.classList.add("hide");
-        calcBody.classList.add("move-calc");
-        setTimeout (function(){
-            calcEmc.style.zIndex = "1";
-        }, 1000)
-
-    } else {
-
+    var containerCalcEmc = document.getElementById("container-calc-emc");
+    if (toggleVisibilidade == true) {
         toggleVisibilidade = false;
-        calcEmc.style.zIndex = "-1";
-        calcEmc.classList.add("move");       
-        
-        setTimeout (function(){
-            menu.classList.remove("hide");
-        }, 800)
-        calcBody.classList.remove("move-calc");
-
+        containerCalcEmc.classList.add("container-show");
+        calcEmc.classList.add("calc-emc-show");
+    } else {
+        toggleVisibilidade = true;
+        containerCalcEmc.classList.remove("container-show");
+        calcEmc.classList.remove("calc-emc-show");       
     }
 }
-
-
 
 function calcEnergia(){
     var massa = document.getElementById("massa");
     console.log(parseFloat(massa.value));
-    var calcEnegia = (parseFloat(massa.value)) * Math.pow(299792458, 2);
+    var calcEnegia = ((parseFloat(massa.value)) * Math.pow(299792458, 2)) / 1000000;
     var energia = document.getElementById("energia");
     console.log(calcEnegia);
-    energia.value = calcEnegia;
-}*/
+    energia.value = calcEnegia.toLocaleString("pt-br");
+}
+
+function calcMassa(){
+    var energia = document.getElementById("energia");
+    console.log(parseFloat(energia.value));
+    var calcMassa = ((parseFloat(energia.value)) / Math.pow(299792458, 2)) * 1000000;
+    var massa = document.getElementById("massa");
+    console.log(calcMassa);
+    massa.value = calcMassa;
+}
